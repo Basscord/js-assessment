d3419dbc24b280494a78e19a092e65a1145020aa
Basscord <brian.mau@brian-mau.com>
2016-06-24 23:47:51 -0700

    All async passed. Raw JS. Maybe review that later

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(
+      function(resolve, reject) {
+        window.setTimeout(
+          function() {
+            resolve(value);
+          }, 10);
+        }
+    );
   },
 
-  manipulateRemoteData: function(url) {
-
+  manipulateRemoteData: function(url) {    
+    return new Promise(
+      function(resolve, reject) {
+        $.ajax(url).then(function(resp) {
+          var people = $.map(resp.people, function(person) {
+            return person.name;
+          });
+          resolve(people.sort());
+        });
+      }
+    );
   }
 };
