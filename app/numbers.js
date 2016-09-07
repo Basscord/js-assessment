061fc0728905240c842966880defede6ccab510e
Basscord <brian.mau@brian-mau.com>
2016-09-07 10:15:08 -0700

    Commit most recent JS

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,51 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    // 128 === 10000000
+    return 1 & (num >> (bit - 1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
 
+    /*
+    expect(numbersAnswers.convertToBinary(128)).to.eql('10000000');
+    expect(numbersAnswers.convertToBinary(65)).to.eql('01000001');
+    */
+
+    /*
+    Which Javascript array functions are mutating?
+    pop, push, reverse, shift, sort, splice, unshift
+    */
+
+    var arr = [];
+    var i = 1;
+    while (i <= 8) {
+        arr.push(this.valueAtBit(num, i));
+        i++;
+    }
+
+    return arr.reverse().join("");
   },
 
   multiply: function(a, b) {
+    a = adjust(a);
+    b = adjust(b);
 
+    var result = (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
+
+    return result;
+    function adjust(num) {
+        var exponent, multiplier;
+        if (num < 1) { // if we're multiplying something less than one, e.g. 0.1
+            exponent = Math.floor(Math.log(num) * -1);
+            multiplier = Math.pow(10, exponent);
+            return {adjusted: num * multiplier, multiplier: multiplier};
+        }
+        return {adjusted: num, multiplier:1};
+    }
   }
 };
--- app/recursion.js
+++ app/recursion.js
@@ -2,15 +2,77 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var result = [];
+    var self = this;
 
+    function doIt(one, two) {
+        var temp = self.listFiles(one, two);
+        for (var i = 0; i < temp.length; i++) {
+            result.push(temp[i]);
+        }
+    }
+
+    for (var i = 0; i < data.files.length; i++) {
+        if((typeof data.files[i] === 'object') && ((!dirName) || (dirName === data.dir) || (dirName === data.files[i].dir))) {
+            doIt(data.files[i], data.files[i].dir);    
+        }
+        else if((!dirName) || (dirName === data.dir)) {
+            result.push(data.files[i]);
+        }
+    }
+
+    return result;
   },
 
   permute: function(arr) {
 
+    var temp = [];
+    var permutations = [];
+    
+    function store() {
+        permutations.push(temp.slice());
+    }
+
+    function moveFromArrayToTemp(index) {
+        var item = arr.splice(index, 1)[0];
+        temp.push(item);
+    }
+
+    function moveFromTempToArray(index) {
+        arr.splice(index, 0, temp.pop());
+    }
+
+    function doIt() {
+
+        for(var index = 0; index < arr.length; index++) {
+            
+            moveFromArrayToTemp(index);
+            
+            if(arr.length) {
+                doIt();
+            } else {
+                store();              
+            }
+
+            moveFromTempToArray(index);
+        }
+        
+    }
+
+    doIt();
+
+    return permutations;
+
   },
 
   fibonacci: function(n) {
-
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return this.fibonacci(n-1) + this.fibonacci(n-2);
   },
 
   validParentheses: function(n) {
